c3929e6ffe8c7136599dc055c5937294606ccdba
Amirhossein <amirabdolii@gmail.com>
2023-07-20 00:34:01 +0330

    fix(phase_2): main has less bloating now

--- Main.java
+++ Main.java
@@ -2,16 +2,41 @@ import java.util.ArrayList;
 
 public class Main {
     public static final String WHITESPACE_REGEX = "\\s+";
+    private static UserInterface console = new Console();
+    private static InvertedIndex ii;
+
+    private static void initializeInvertedIndex(String sourcePath) {
+        FileReader fileReader = new FileReader(sourcePath);
+        ArrayList<String> filenames = fileReader.getFilenames();
+        for (String filename : filenames)
+            ii.addDocument(fileReader.getFullText(filename), StringUtils.removeAfter(filename, '.'));
+    }
+
+    private static Filter initializeFilter(Filter filter, ArrayList<String> filterOptions) {
+        for (String filterOption : filterOptions)
+            filter.addData(ii.searchDocuments(filterOption));
+        return filter;
+    }
+
+    private static ArrayList<String> applyFilters(ArrayList<String> initialResult, Filter[] filters) {
+        ArrayList<String> finalResult = new ArrayList<String>(initialResult);
+        for (Filter filter : filters)
+            finalResult = filter.filter(finalResult);
+        return finalResult;
+    }
+
+    private static void ShowSearchResult(ArrayList<String> searchResult) {
+        if (searchResult.size() != 0)
+            console.printOrderedArray(searchResult);
+        else
+            console.println("<-No Result->");
+    }
 
     public static void main(String[] args) {
         UserInterface console = new Console();
         console.println("Wait for the preprocessing...");
-        InvertedIndex ii = new InvertedIndex(new RegexTokenizer(WHITESPACE_REGEX + "|:|-"), new TestNormalizer());
-
-        FileReader fileReader = new FileReader("./documents");
-        ArrayList<String> filenames = fileReader.getFilenames();
-        for (String filename : filenames)
-            ii.addDocument(fileReader.getFullText(filename), StringUtils.removeAfter(filename, '.'));
+        ii = new InvertedIndex(new RegexTokenizer(WHITESPACE_REGEX + "|:|-"), new TestNormalizer());
+        initializeInvertedIndex("./documents");
 
         Tokenizer wsTokenizer = new RegexTokenizer(WHITESPACE_REGEX);
         while (true) {
@@ -20,29 +45,18 @@ public class Main {
             ArrayList<String> andOptions, orOptions, notOptions;
 
             orOptions = options.pop('+');
-            Filter orFilter = new OrFilter();
-            for (String orOption : orOptions)
-                orFilter.addData(ii.searchDocuments(orOption));
+            Filter orFilter = initializeFilter(new OrFilter(), orOptions);
 
             notOptions = options.pop('-');
-            Filter notFilter = new NotFilter();
-            for (String notOption : notOptions)
-                notFilter.addData(ii.searchDocuments(notOption));
+            Filter notFilter = initializeFilter(new NotFilter(), notOptions);
 
             andOptions = options.getRemained();
-            Filter andFilter = new AndFilter(ii.searchDocuments(andOptions.get(0)));
-            for (String andOption : andOptions)
-                andFilter.addData(ii.searchDocuments(andOption));
+            Filter andFilter = initializeFilter(new AndFilter(ii.searchDocuments(andOptions.get(0))), andOptions);
 
-            ArrayList<String> searchResult = ii.searchDocuments(andOptions.get(0));
+            ArrayList<String> searchResult = applyFilters(ii.searchDocuments(andOptions.get(0)),
+                    new Filter[] { andFilter, orFilter, notFilter });
 
-            for (Filter filter : new Filter[] { orFilter, andFilter, notFilter })
-                searchResult = filter.filter(searchResult);
-
-            if (searchResult.size() != 0)
-                console.printOrderedArray(searchResult);
-            else
-                console.println("<-No Result->");
+            ShowSearchResult(searchResult);
         }
     }
 }
