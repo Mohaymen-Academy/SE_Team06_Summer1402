1a87fb598fce1eb57550b0075d180aa9a8a28281
Amirhossein <amirabdolii@gmail.com>
2023-07-19 12:42:38 +0330

    feat(phase_2): filter class implemented

---
+++ Filter.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public interface Filter {
+    public ArrayList<String> filter(ArrayList<String> data);
+
+    public void addData(ArrayList<String> newData);
+}
+
+class AndFilter implements Filter {
+    private ArrayList<String> _data;
+
+    public AndFilter(ArrayList<String> data) {
+        _data = data;
+    }
+
+    public ArrayList<String> getData() {
+        return _data;
+    }
+
+    public ArrayList<String> filter(ArrayList<String> data) {
+        ArrayList<String> result = new ArrayList<String>();
+        for (String s : data)
+            if (_data.contains(s))
+                result.add(s);
+        return result;
+    }
+
+    public void addData(ArrayList<String> data) {
+        _data = filter(data);
+    }
+
+    
+}
